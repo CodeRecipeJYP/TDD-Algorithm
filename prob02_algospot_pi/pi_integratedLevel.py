798903aadfc4e1ddc1a2ef080f593c50a53ae27c
pjo901018 <pjo901018@korea.ac.kr>
2018-02-14 09:44:34 +0900

    Passed integratedLevel tests

--- prob02_algospot_pi/pi_integratedLevel.py
+++ prob02_algospot_pi/pi_integratedLevel.py
@@ -1,2 +1,20 @@
+from prob02_algospot_pi.pi import *
+
+
 def integratedLevel(numbers: str, sublength: int):
-    pass
+    if (subSameNumber(numbers, sublength)):
+        return 1
+
+    if (subSimpleIncrease(numbers, sublength)):
+        return 2
+
+    if (subSimpleDecrease(numbers, sublength)):
+        return 2
+
+    if (subRotate(numbers, sublength)):
+        return 4
+
+    if (subArithmeticSequence(numbers, sublength)):
+        return 5
+
+    return 10
