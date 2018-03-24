4d4e398fc9ad19a86b5626d53ecbe2bb9ac737dc
kujyp <pjo901018@gmail.com>
2018-03-24 18:22:24 +0900

    Implement with Dijkstra's algorithm

--- probs/prob05_routing/routing.py
+++ probs/prob05_routing/routing.py
@@ -26,6 +26,9 @@
 """
 
 
+INF = 999999
+
+
 def str2int_or_float_array(string):
     splitted = string.split(" ")
     array = []
@@ -52,7 +55,33 @@ def empty_2darr(w, h):
 
 
 def lowest_route_noiseratio(graph_table):
-    return ""
+    count_computers = len(graph_table)
+
+    visited_s = [0]
+    noise_d = [INF] * count_computers
+    noise_d[0] = 1
+    notvisited_q = [computer for computer in range(0, count_computers)]
+
+    while len(notvisited_q) != 0:
+        minimum = INF
+        minimum_q_computer = -1
+        for q_computer in notvisited_q:
+            if minimum > noise_d[q_computer]:
+                minimum = noise_d[q_computer]
+                minimum_q_computer = q_computer
+
+        for dst_computer, weight in enumerate(graph_table[minimum_q_computer]):
+            if weight == 0:
+                continue
+
+            found_noise = noise_d[minimum_q_computer] * weight
+            if noise_d[dst_computer] > found_noise:
+                noise_d[dst_computer] = found_noise
+
+        visited_s.append(minimum_q_computer)
+        notvisited_q.remove(minimum_q_computer)
+
+    return noise_d[-1]
 
 
 def main():
@@ -63,7 +92,12 @@ def main():
         graph_table = empty_2darr(count_computers, count_computers)
         for edge_idx in range(0, count_edges):
             [st, dst, weight] = str2int_or_float_array(input())
-            graph_table[st][dst] = weight
+            if graph_table[st][dst] != 0 \
+                    and graph_table[st][dst] <= weight:
+                continue
+            else:
+                graph_table[st][dst] = weight
+                graph_table[dst][st] = weight
 
         print(lowest_route_noiseratio(graph_table))
 
