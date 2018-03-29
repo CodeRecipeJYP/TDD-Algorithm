84aacb66f8e10bd340782afd857161710af31268
kujyp <pjo901018@gmail.com>
2018-03-29 17:10:44 +0900

    Implment get_curr_ranks

--- probs/prob13_finalrank/finalrank.py
+++ probs/prob13_finalrank/finalrank.py
@@ -14,8 +14,30 @@ def empty_2darr(num_row, num_col):
     return [[0 for _ in range(num_col)] for _ in range(num_row)]
 
 
+RESULT_IMPOSSIBLE = ["IMPOSSIBLE"]
+
+
+def make_graph_table(prev_ranks):
+    return []
+
+
+def update_diffs(graph_table, diffs):
+    return []
+
+
 def get_curr_ranks(team_count, prev_ranks, diffs):
-    return prev_ranks
+    graph_table = make_graph_table(prev_ranks)
+    curr_graph_table = update_diffs(graph_table, diffs)
+
+    curr_ranks = []
+    for each_row in curr_graph_table:
+        curr_rank = (team_count - 1) - sum(each_row)
+        if curr_rank not in curr_ranks:
+            curr_ranks.append(curr_rank)
+        else:
+            return RESULT_IMPOSSIBLE
+
+    return curr_ranks
 
 
 def print_list_on_single_line(lists):
