046d88183f052e03e2d1de654ab48304e307acba
kujyp <pjo901018@gmail.com>
2018-03-29 14:24:24 +0900

    Write pseudo code

--- probs/prob12_tomato/tomato.py
+++ probs/prob12_tomato/tomato.py
@@ -29,8 +29,41 @@ def copy_2darr(arr):
     return copy
 
 
+RESULT_IMPOSSIBLE = -1
+
+
 def get_days_everytomato_gonnabe_ripen(tomato_map):
-    return 0
+    elapsed_days = 0
+
+    while True:
+        will_be_ripen_tomatos = get_will_be_ripen_tomatos(tomato_map)
+
+        if len(will_be_ripen_tomatos) == 0:
+            if is_everytomato_ripen(tomato_map):
+                break
+            else:
+                return RESULT_IMPOSSIBLE
+
+        tomato_map = update_ripen_tomatos(tomato_map, will_be_ripen_tomatos)
+        elapsed_days += 1
+
+    return elapsed_days
+
+
+def update_ripen_tomatos(tomato_map, will_be_ripen_tomatos):
+    return tomato_map
+
+
+def is_everytomato_ripen(tomato_map):
+    return False
+
+
+def get_will_be_ripen_tomatos(tomato_map):
+    return []
+
+
+def get_around_tomatos_will_be_ripen(tomato_map, tomato):
+    return []
 
 
 def main():
