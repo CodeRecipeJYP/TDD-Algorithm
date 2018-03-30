4060b7737c326a31ad27848860ffd27d7a3aefba
kujyp <pjo901018@gmail.com>
2018-03-30 16:24:41 +0900

    Remove validation of same route edges

--- probs/prob14_shortestpath/shortestpath.py
+++ probs/prob14_shortestpath/shortestpath.py
@@ -20,8 +20,8 @@ INF_TEXT = "INF"
 NONE_PREVIOUS_VERTEX = -1
 
 
-def shortest_path_weight(start_vertex_number, graph_table):
-    vertex_count = len(graph_table)
+def shortest_path_weight(start_vertex_number, edges):
+    vertex_count = len(edges)
 
     start_vertex_idx = start_vertex_number - 1
 
@@ -40,7 +40,7 @@ def shortest_path_weight(start_vertex_number, graph_table):
         if distance > d[vertex_idx]:
             continue
 
-        for dst, weight in graph_table[vertex_idx].items():
+        for dst, weight in edges[vertex_idx]:
             found_weight = d[vertex_idx] + weight
             if d[dst] > found_weight:
                 pq_item = (found_weight, dst, vertex_idx)
@@ -68,20 +68,16 @@ def main():
     vertex_count, edge_count = str2int_array(input())
     start_vertex_number = int(input())
 
-    graph_table = [{} for _ in range(vertex_count)]
+    edges = [[] for _ in range(vertex_count)]
 
     for _ in range(edge_count):
         st, dst, weight = str2int_array(input())
 
-        st_idx, dst_idx_key = st - 1, dst - 1
-        if dst_idx_key in graph_table[st_idx] \
-                and graph_table[st_idx][dst_idx_key] <= weight:
-            continue
-        else:
-            graph_table[st_idx][dst_idx_key] = weight
+        st_idx, dst_idx = st - 1, dst - 1
+        edges[st_idx].append((dst_idx, weight))
 
     print_list_on_multi_line(
-        get_solution(start_vertex_number, graph_table))
+        get_solution(start_vertex_number, edges))
 
 
 if __name__ == '__main__':
