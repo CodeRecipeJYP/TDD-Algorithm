883d26a772585c35c502927e4028a82bf4a527d2
kujyp <pjo901018@gmail.com>
2018-04-06 10:40:59 +0900

    Passed all tests

--- java/test/probs/samsung/day04/prob23/TrailTest.java
+++ java/test/probs/samsung/day04/prob23/TrailTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.Assert.*;
@@ -28,6 +30,27 @@ public class TrailTest {
                         caze.input.kSize));
     }
 
+    @Test
+    public void getLongestTrail_case03() throws FileNotFoundException {
+        List<Case> cazes = getCases("case03");
+
+        for (int caseIdx = 0; caseIdx < cazes.size(); caseIdx++) {
+            Case caze = cazes.get(caseIdx);
+            assertEquals(caze.output,
+                    Trail.getLongestTrail(caze.input.map,
+                            caze.input.kSize));
+        }
+    }
+
+    @Test
+    public void getLongestTrail_case04() throws FileNotFoundException {
+        Case caze = getCase("case04");
+
+        assertEquals(caze.output,
+                Trail.getLongestTrail(caze.input.map,
+                        caze.input.kSize));
+    }
+
     private Case getCase(String filename) throws FileNotFoundException {
         String prefix = "test/probs/samsung/day04/prob23/" + filename;
         String inputPath = prefix + ".in";
@@ -36,6 +59,36 @@ public class TrailTest {
         int expected = getOutput(new Scanner(new File(outputPath)));
 
         return new Case(input, expected);
+    }
+
+    private List<Case> getCases(String filename) throws FileNotFoundException {
+        String prefix = "test/probs/samsung/day04/prob23/" + filename;
+        String inputPath = prefix + ".in";
+        String outputPath = prefix + ".out";
+        List<Trail.Input> inputs = Trail.getInputs(new Scanner(new File(inputPath)));
+        List<Integer> outputs = getOutputs(new Scanner(new File(outputPath)));
+
+        List<Case> cazes = new ArrayList<>();
+
+        for (int caseIdx = 0; caseIdx < inputs.size(); caseIdx++) {
+            Trail.Input input = inputs.get(caseIdx);
+            int expected = outputs.get(caseIdx);
+            cazes.add(new Case(input, expected));
+        }
+
+        return cazes;
+    }
+
+    private List<Integer> getOutputs(Scanner scanner) {
+        List<Integer> result = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            String s = scanner.next();
+
+            result.add(scanner.nextInt());
+            scanner.nextLine();
+        }
+
+        return result;
     }
 
     private int getOutput(Scanner scanner) {
