758b368719cdcd8d510fbe1d38a6c21a05ceb169
kujyp <pjo901018@gmail.com>
2018-04-17 22:29:41 +0900

    passed test case

--- probs/prob27/naver.py
+++ probs/prob27/naver.py
@@ -1,5 +1,17 @@
 def dash_insert(string):
-    return ""
+    result = string[0]
+    state = int(string[0]) % 2
+    for char in string[1:]:
+        curr = int(char) % 2
+        if state == curr:
+            if state == 0:
+                result += "+"
+            else:
+                result += "-"
+
+        result += char
+        state = curr
+    return result
 
 
 def main():
