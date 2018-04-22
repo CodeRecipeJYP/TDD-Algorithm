849a9f8085637289de54f5d14061b28cdfd5955e
kujyp <pjo901018@korea.ac.kr>
2018-04-22 13:52:45 +0900

    Add linebyline case generator / Add case outputs

--- java/src/test/java/probs/prob31/StatMainTest.java
+++ java/src/test/java/probs/prob31/StatMainTest.java
@@ -12,9 +12,35 @@ import static probs.RunAllSuite.TEST_PROJECT_ROOT;
 public class StatMainTest {
 
     @Test
-    public void main() {
-        String filepath = TEST_PROJECT_ROOT + "/prob31/" + "case01.in";
+    public void case01() {
+        String filepath;
+        filepath = TEST_PROJECT_ROOT + "/prob31/" + "case01.in";
+
+        try {
+            System.setIn(new FileInputStream(filepath));
+        } catch (FileNotFoundException ignore) {
+        }
+
+        StatMain.main(null);
+    }
+
+    @Test
+    public void case02() {
+        String filepath;
         filepath = TEST_PROJECT_ROOT + "/prob31/" + "case02.in";
+
+        try {
+            System.setIn(new FileInputStream(filepath));
+        } catch (FileNotFoundException ignore) {
+        }
+
+        StatMain.main(null);
+    }
+
+    @Test
+    public void case03() {
+        String filepath;
+        filepath = TEST_PROJECT_ROOT + "/prob31/" + "case03.in";
 
         try {
             System.setIn(new FileInputStream(filepath));
